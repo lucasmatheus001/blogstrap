c16f3990896aebb21c29b3ac85cfaa9df7ff756c
Lucas Rocha <lucas.rocha@maida.health>
2022-01-30 21:13:53 -0300

    Adicionando a exibicao de articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+    # pega valor e joga na url params
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    def index
+        @articles = Article.all
+    end
 end
